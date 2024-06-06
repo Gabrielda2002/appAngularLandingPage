52f33c71c2bb480e4aa7a366f55d1a1ad0808275
Gabriel Duarte <duartearias2002@gmail.com>
2024-06-06 17:44:20 -0500

    haciendo formularios reactivos

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgModel, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  formularioContacto: FormGroup;
+  tipoDni: string = 'DNI';
 
+  constructor(private form: FormBuilder) {
+    this.formularioContacto = this.form.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(3)]],
+      dni: [''],
+      tipoDni: [''],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  // * otra manera de setear los validators en el ngOnInit
+
+  ngOnInit(): void {
+    // ? this.formularioContacto.get('apellido')?.setValidators([Validators.required, Validators.minLength(3)]);
+    this.formularioContacto.get('tipoDni')?.valueChanges.subscribe(value =>{
+      this.tipoDni = value;
+      // console.log(this.tipoDni);
+    })
+  }
+
+  hasErrors(controlName: string, errorType: string) {
+    return (
+      this.formularioContacto.get(controlName)?.hasError(errorType) &&
+      this.formularioContacto.get(controlName)?.touched
+    );
+  }
+
+  enviar() {
+    console.log(this.formularioContacto);
+  }
 }
